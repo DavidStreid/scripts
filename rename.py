cd915ca8ee21268b860fa226fb846efaaf070f35
Streid <streidd@lski2780.mskcc.org>
2019-07-26 14:21:41 -0400

    will remove igo_dic representation

--- rename.py
+++ rename.py
@@ -1,14 +1,21 @@
 import sys
 import os
+from shutil import copyfile
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 from shutil import copyfile
 
+# TODO - lint
+
 NUM_COLUMNS = 73
 DESTINATION = './'
+SAMPLE = 'S0000'        # TODO - looks like this is set by the nikon macro
+IGO_DIR_LOCATION = os.getcwd() # TODO - allow user to specify
 
 '''
+    igoDir - Tracks the structure of our IGO Directory. 
+
     {
         run: {
             sample: {
@@ -20,47 +27,81 @@ DESTINATION = './'
 
     }
 '''
-files = {}
+igo_dir = {}  
 
-def getRowColumn(path):
-    extension = '.txt'
-    location = './'
+def get_row_column(path):
+    extension = '.tif'      # TODO
+    location = './'         # TODO
     file = path.strip(extension).strip(location)
     attr = file.split('_')
     if(len(attr) != 3):
-        raise ValueError('Could not parse %s' % path)
+        raise ValueError('Created file should have format [POS][RUN]_[COL]_[ROW]: %s' % path)
     row = int(attr[2])
     column = NUM_COLUMNS - int(attr[1])
     run = attr[0][-2:]
     return [row,column,run]
 
-def copyFileToFolder(path,row,col,run):
-    row = '0%d' % row if row < 10 else str(row)
-    col = '0%d' % col if col < 10 else str(col)
-    name = 'R%s_C%s_0000_00_%s.tif' % (row,col,run)
+def put_directory_if_absent(dic, key, val, path, rsc):
+    """
+    Adds directory if absent in IGO directory. Returns state of directory mapped by key
+
+    Args:
+      dic (dict: str:dict/set): representation of directory
+      key (str): file/directory name
+      val (empty dict/set): Default value mapped by key if key doesn't exist
+    """
+
+    files = os.listdir(path)
+    '''
+    if key in dic:
+        return dic[key]
+    else:
+    '''
+    if key not in files:
+        # TODO - make directory in correct location
+        new_path = '%s/%s' % (path,rsc)
+        os.mkdir(new_path)
+        dic[key] = val
+        return dic[key]
+    else:
+        print("IMPORTANT - %s was at path %s" % (key, path))
+
+def copyFileToIgoDir(path,row,col,run):
+    row = 'R' + ('0%d' % row if row < 10 else str(row))
+    col = 'C' + ('0%d' % col if col < 10 else str(col))
+    name = '%s_%s_0000_00_%s.tif' % (row,col,run)
     dest = DESTINATION + name
 
-    '''
-    logic to make directories
-    '''
+    dirPath = IGO_DIR_LOCATION
+    runs = put_directory_if_absent(igo_dir, run, {},dirPath,run)
+    dirPath = '%s/%s' % (IGO_DIR_LOCATION,run)
+    samples = put_directory_if_absent(runs, SAMPLE, {},dirPath,SAMPLE)
+    dirPath = '%s/%s' % (dirPath,SAMPLE)
+    columns = put_directory_if_absent(samples, col, set(),dirPath,col)
+    dirPath = '%s/%s' % (dirPath,col)
 
-    copyfile(path, dest)
+    # Check that name is not being added
+    dirPath = '%s/%s' % (dirPath,name)
+    copyfile(path,dirPath)
+    columns.add(name)
+
+    print(igo_dir)
 
 class EventHandler(PatternMatchingEventHandler):
     def on_created(self, event):
         try:
             path =  event.src_path
-            [row,column,run] = getRowColumn(path)
-            copyFileToFolder(path,row,column,run)
+            [row,column,run] = get_row_column(path)
+            copyFileToIgoDir(path,row,column,run)
         except ValueError as err:
-            print err
+            print(err)
             return
     def on_moved(self, event):
-        print ('File moved: %s' % event.src_path)
+        print ('File moved - %s' % event.src_path)
     def on_deleted(self, event):
-        print ('File deleted: %s' % event.src_path)
+        print ('ERROR: File deleted - %s' % event.src_path)
     def on_modified(self, event):
-        print ('File modified: %s' % event.src_path)
+        print ('File modified - %s' % event.src_path)
 
 def getHandler():
     patterns = "*"
