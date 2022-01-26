ee567c3ae590df9a7415e44d39976f869b5eef5f
David Streid <david.streid@variantyx.com>
2022-01-26 13:25:50 -0500

    Adding graph function

---
+++ analysis/graph_nums.py
@@ -0,0 +1,13 @@
+def graph(list_of_nums, title):
+  '''Creates bar graph
+  :param list_of_nums, float[]
+  :param title, str
+  '''
+  num_bins = 10
+  plt.hist(list_of_nums, num_bins, facecolor='blue', alpha=0.5)
+  plt.title(title)
+  plt.xlabel('My numbers')
+  plt.ylabel('Count')   
+  fname = "_".join(title.split(" "))
+  plt.savefig("%s.pdf" % fname)
+  plt.close()
