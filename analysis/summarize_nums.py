4e7319c9a353fd95520843cf7bb102593a621e16
David Streid <david.streid@variantyx.com>
2022-02-01 16:42:28 -0500

    fix

--- analysis/summarize_nums.py
+++ analysis/summarize_nums.py
@@ -6,7 +6,7 @@ f = sys.argv[1] # e.g. 'for i in {1..10}; do echo ${i}; done > f.txt'
 counts = []
 with open(f, 'r') as in_f:
   for line in in_f:
-    num = int(line.strip())
+    num = float(line.strip())
     counts.append(num)
 
 s = pd.Series(counts)
