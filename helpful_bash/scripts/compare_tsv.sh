4d609285407983ed209ef277fd5917d20a49b52a
DavidStreid <david.streid.nyc@gmail.com>
2026-08-27 10:21:46 -0400

    fixing diff logging
    was somehow grabbing the blank space for the count of differences, which is always less than 0

--- helpful_bash/scripts/compare_tsv.sh
+++ helpful_bash/scripts/compare_tsv.sh
@@ -22,26 +22,23 @@ echo "Sorting ${base_v2} -> ${V2_SORTED}"
 head -1 ${V2} > ${V2_SORTED}
 tail -n+2 ${V2} | sort >> ${V2_SORTED}
 
-# Get column counts for both files
+# Grab the minimum column count to compare between the two files
 num_cols1=$(awk -F'\t' 'NR==1{print NF}' "${V1}")
 num_cols2=$(awk -F'\t' 'NR==1{print NF}' "${V2}")
+(( min_cols = num_cols1 < num_cols2 ? num_cols1 : num_cols2 )) 
 
-# Calculate the minimum using a ternary arithmetic evaluation
-(( min_cols = num_cols1 < num_cols2 ? num_cols1 : num_cols2 ))
+echo "Comparing up to column ${min_cols} (new: ${num_cols1}, old: ${num_cols2})"
 
-echo "Comparing up to column ${min_cols}..."
-
-echo "COMPARING, n=${min_cols} (new: ${num_cols1}, old: ${num_cols2})"
 for ((i=1; i<=min_cols; i++)); do
   c_col=$(head -1 ${V2_SORTED} | cut -f${i})
   o_col=$(head -1 ${V1_SORTED} | cut -f${i})
   out=col${i}.txt
   diff <(cut -f${i} ${V1_SORTED}) <(cut -f${i} ${V2_SORTED}) > ${out}
-  d=$(wc -l ${out} | sed 's/  */\t/g' | cut -f1)
+  d=$(wc -l ${out} | sed 's/^  *//g' | sed 's/  */\t/g' | cut -f1)
   if [[ ${d} -gt 0 ]]; then
     printf "${i}\t${c_col}\t${o_col}\tDIFF\n"
   else
     printf "${i}\t${c_col}\t${o_col}\tSAME\n"
     rm ${out}
   fi
-done
+done 2>&1 | tee comparison.tsv
